e565f2a530898d4728b0a35e13a26cb1c5062897
Mostafa El-Diasty <mostafaeldiasty95@gmail.com>
2021-10-08 14:18:48 +0200

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        List<Movie> movies = new List<Movie>{
+                new Movie{Id=1,Name = "Shrek"},
+                new Movie{Id=2,Name = "Wall-e"}
+            };
+        //
+        // GET: /Movies/
+        public ActionResult Index()
+        {
+            return View(movies);
+        }
+	}
+}
